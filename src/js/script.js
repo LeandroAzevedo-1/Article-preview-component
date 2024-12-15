e38a38f2706b56e8ae153f946890e4880abde5e6
perkles <leoazevedo1450@gmail.com>
2024-12-15 00:27:52 -0300

    Tarefa: Css Responsivo, em construção...

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const btnSeta = document.querySelector('.btn-seta');
 const socialMedia = document.querySelector('.social-media');
 
+
 btnSeta.addEventListener('click', () => {
     socialMedia.classList.toggle('active');
 });

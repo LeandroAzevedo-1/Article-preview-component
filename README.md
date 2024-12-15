8b92f31f0bfac45863990d68e0c4a60f336332db
perkles <leoazevedo1450@gmail.com>
2024-12-14 21:39:31 -0300

    Tarefa: Adicionando JavaScript ao projeto

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Frontend Mentor - Article preview component
 
 ## Projeto em construção.
+
+
+
+Acesse a página online: [Live Demo](https://leandroazevedo-1.github.io/Article-preview-component/).
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 const btnSeta = document.querySelector('.btn-seta');
 const socialMedia = document.querySelector('.social-media');
 
-// Adicionar um evento de click que adicione e remover o display flex ao social media
 btnSeta.addEventListener('click', () => {
-    
     socialMedia.classList.toggle('active');
-})
+});
